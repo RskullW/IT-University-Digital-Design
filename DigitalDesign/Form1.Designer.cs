f999d8900008635871f4355ae8bef1668651e58b
RskullW <102870472+RskullW@users.noreply.github.com>
2022-09-29 01:50:40 +0300

    first commit

---
+++ DigitalDesign/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DigitalDesign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.HelpMessage = new System.Windows.Forms.TextBox();
+            this.CreateButton = new System.Windows.Forms.Button();
+            this.InputN = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.HelpMessage);
+            this.panel1.Controls.Add(this.CreateButton);
+            this.panel1.Controls.Add(this.InputN);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 450);
+            this.panel1.TabIndex = 0;
+            // 
+            // HelpMessage
+            // 
+            this.HelpMessage.BackColor = System.Drawing.SystemColors.MenuBar;
+            this.HelpMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.HelpMessage.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.HelpMessage.Location = new System.Drawing.Point(199, 76);
+            this.HelpMessage.Multiline = true;
+            this.HelpMessage.Name = "HelpMessage";
+            this.HelpMessage.Size = new System.Drawing.Size(521, 23);
+            this.HelpMessage.TabIndex = 2;
+            this.HelpMessage.Text = "Введите число N (количество линий до 10)";
+            // 
+            // CreateButton
+            // 
+            this.CreateButton.Location = new System.Drawing.Point(298, 157);
+            this.CreateButton.Name = "CreateButton";
+            this.CreateButton.Size = new System.Drawing.Size(223, 42);
+            this.CreateButton.TabIndex = 1;
+            this.CreateButton.Text = "Создать сейф";
+            this.CreateButton.UseVisualStyleBackColor = true;
+            // 
+            // InputN
+            // 
+            this.InputN.Location = new System.Drawing.Point(342, 105);
+            this.InputN.Name = "InputN";
+            this.InputN.Size = new System.Drawing.Size(133, 22);
+            this.InputN.TabIndex = 0;
+            this.InputN.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InputN_KeyPress);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.TextBox HelpMessage;
+
+        private System.Windows.Forms.TextBox textBox2;
+
+        private System.Windows.Forms.TextBox InputN;
+        private System.Windows.Forms.Button CreateButton;
+
+        private System.Windows.Forms.Panel panel1;
+
+        #endregion
+    }
+}
---
+++ DigitalDesign/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DigitalDesign
+{
+    public partial class Form1 : Form
+    {
+        private int _numberOfLine;
+        private bool _isCreate;
+        public Form1()
+        {
+            Awake();
+        }
+        private void Awake()
+        {
+            try
+            {
+                InitializeComponent();
+                InitializeTextLabels();
+                InitializeStartButton();
+            }
+
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+        private void InitializeTextLabels()
+        {
+            HelpMessage.ReadOnly = true;
+        }
+        private void InitializeStartButton()
+        {
+            CreateButton.Click += ClickButton;
+        }
+        private void ClickButton(object sender, EventArgs e)
+        {
+            _numberOfLine = Convert.ToInt32(InputN.Text);
+
+            if (_numberOfLine >= 10)
+            {
+                _numberOfLine = 9;
+            }
+            
+            InputN.Enabled = InputN.Visible = false;
+            HelpMessage.Enabled = HelpMessage.Visible = false;
+            CreateButton.Enabled = CreateButton.Visible = false;
+            
+            CreateLines();
+        }
+        private void InputN_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+            if (!Char.IsDigit(e.KeyChar) && e.KeyChar != 8)
+            {
+                e.Handled = true;
+            }
+        }
+        private void CreateLines()
+        {
+            if (_isCreate)
+            {
+                throw new Exception("Warning! You have already created the lines!");
+            }
+
+            _isCreate = true;
+
+
+            for (int i = 0; i < _numberOfLine; ++i)
+            {
+                ushort numbersEven = 0, numbersOdd = 0;
+
+                for (int j = 0; j < _numberOfLine; ++j)
+                {
+                    Button btn = new Button();
+                    btn.Name = i + j.ToString();
+                    btn.Location = new Point(Width / 4 + 40 * i, Height / 10 + j * 30);
+
+                    if (PlaceRandomly() % 2 == 0)
+                    {
+                        btn.Size = new Size(10, 25);
+                        numbersEven++;
+                    }
+
+                    else
+                    {
+                        numbersOdd++;
+                        btn.Size = new Size(25, 10);
+                    }
+
+                    if (numbersEven * 2 >= _numberOfLine || numbersOdd * 2 >= _numberOfLine)
+                        btn.Size = new Size(btn.Height, btn.Width);
+
+                    panel1.Controls.Add(btn);
+                    btn.Click += RotateLines;
+                }
+            }
+        }
+        private ushort PlaceRandomly()
+        {
+            var random = new Random();
+            return (ushort)random.Next(999);
+        }
+        private void RotateLines(object sender, EventArgs e)
+        {
+            var button = (Button)sender;
+            int i = int.Parse(button.Name[0].ToString());
+            int j = int.Parse(button.Name[1].ToString());
+
+            button.Size = new Size(button.Height, button.Width);
+            for (int index = 0; index < _numberOfLine; ++index)
+            {
+                button = panel1.Controls[i.ToString() + index] as Button;
+                if (button != null) button.Size = new Size(button.Height, button.Width);
+                button = panel1.Controls[index.ToString() + j] as Button;
+                if (button != null) button.Size = new Size(button.Height, button.Width);
+            }
+
+            ProcessRotation();
+        }
+        private void ProcessRotation()
+        {
+            for (int i = 0; i < _numberOfLine; ++i)
+            {
+                for (int j = 0; j < _numberOfLine-1; ++j)
+                {
+                    Button firstButton = panel1.Controls[i.ToString() + j] as Button;
+                    Button secondButton = panel1.Controls[i.ToString() + (j+1)] as Button;
+                    if (firstButton != null && secondButton != null && firstButton.Height != secondButton.Height)
+                    {
+                        return;
+                    }
+                }
+            }
+
+            MessageBox.Show("You win!!!");
+            Application.Restart();
+        }
+    }
+}
